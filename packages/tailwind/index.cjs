e2f6a50f01713dd7965d759523bee693855f480c
kafeindev <kafein40@gmail.com>
2024-05-04 21:44:31 +0300

    feat: add color css variables

--- packages/tailwind/index.cjs
+++ packages/tailwind/index.cjs
@@ -1,5 +1,13 @@
 module.exports = {
-  important: true,
   darkMode: ["class"],
+  theme: {
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        sidebar: "hsl(var(--sidebar))",
+      },
+    },
+  },
   plugins: [require("tailwindcss-animate")],
 };
