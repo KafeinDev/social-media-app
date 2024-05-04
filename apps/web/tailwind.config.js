f00d3fd846525cf40c10eb27ea5cd617b3741898
kafeindev <kafein40@gmail.com>
2024-05-04 21:43:30 +0300

    fix(ui): fix tailwind not working

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,6 +1,11 @@
-import sharedConfig from "@repo/tailwind";
+import sharedConfig from "@repo/ui/tailwind.config.cjs";
 
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  presets: [sharedConfig],
+  ...sharedConfig,
+  content: [
+    ...sharedConfig.content,
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "../../packages/ui/components/**/*.{js,ts,jsx,tsx}",
+  ],
 };
--- packages/ui/tailwind.config.cjs
+++ packages/ui/tailwind.config.cjs
@@ -1,6 +1,5 @@
 import sharedConfig from "@repo/tailwind/index.cjs";
 
 module.exports = {
-  content: ["./components/**/*.{js,ts,jsx,tsx}"],
-  presets: [sharedConfig],
+  ...sharedConfig,
 };
