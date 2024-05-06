60890631b4288a61366a8483bdef9a805b871645
kafeindev <kafein40@gmail.com>
2024-05-06 16:05:01 +0300

    feat(tailwind&ui): add new color css variables

--- packages/tailwind/index.cjs
+++ packages/tailwind/index.cjs
@@ -4,7 +4,17 @@ module.exports = {
     extend: {
       colors: {
         background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
+        foreground: {
+          DEFAULT: "hsl(var(--foreground))",
+          alt: "hsl(var(--foreground-alt))",
+          "alt-2": "hsl(var(--foreground-alt-2))",
+        },
+        primary: "hsl(var(--primary))",
+        secondary: "hsl(var(--secondary))",
+        border: {
+          DEFAULT: "hsl(var(--border))",
+          alt: "hsl(var(--border-alt))",
+        },
         sidebar: "hsl(var(--sidebar))",
       },
     },
